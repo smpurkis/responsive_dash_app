287b8b864363d15a35a2033d42288cc8d6b72026
Sam Purkis <sam.purkis@hotmail.co.uk>
2021-11-22 23:16:03 +0000

    add center styles

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import html, dcc
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -13,22 +12,50 @@ app = dash.Dash(
 
 server = app.server
 
+center_style = {
+    "text-align": "center",
+    "padding": "1em",
+    "max-width": "100%",
+    "margin": "0 auto"
+}
+
+button_style = {
+    "text-align": "center",
+    "max-width": "20rem",
+    "margin": "0 auto"
+}
+
+
+h1_style = {
+    "color": "#ff3e00",
+    "text-transform": "uppercase",
+    "font-size": "4em",
+    "font-weight": "100"
+}
+
+img_style = {
+    "text-align": "center",
+    "margin": "0 auto"
+}
+
 app.layout = html.Div([
-    html.H2('Hello World'),
+    html.H2('Hello World', style=h1_style),
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
+        value='LA',
+        style=button_style
     ),
     html.Div(id='display-value'),
-    html.Img(id="example-image", src="https://placekitten.com/408/287")
-])
+    html.Br(),
+    html.Img(id="example-image", src="https://placekitten.com/408/287", style=img_style)
+], style=center_style)
 
 
 @app.callback(dash.dependencies.Output('display-value', 'children'),
               [dash.dependencies.Input('dropdown', 'value')])
 def display_value(value):
-    return 'You have selected "{}"'.format(value)
+    return f'You have selected "{value}"'
 
 
 if __name__ == '__main__':
